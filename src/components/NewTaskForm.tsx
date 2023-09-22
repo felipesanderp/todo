4f4fb89058b11098066a761aa7fd4328f8c8fe17
felipesanderp <felipe.panisson@hotmail.com>
2023-09-22 11:23:00 -0300

    add new task

--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -1,18 +1,43 @@
+import { ChangeEvent, FormEvent, useContext, useState } from 'react'
 import { PlusCircle } from 'phosphor-react'
 
 import styles from './NewTaskForm.module.css'
 
+import { TasksContext } from '../contexts/TasksContext'
+
 export function NewTaskForm() {
+  const [newTaskText, setNewTaskText] = useState('')
+
+  const { addTask } = useContext(TasksContext)
+
+  function handleAddNewTask(event: FormEvent) {
+    event.preventDefault()
+    
+    addTask({
+      content: newTaskText,
+      isCompleted: false,
+    })
+    setNewTaskText('')
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setNewTaskText(event.target.value)
+  }
+
   return (
-    <div className={styles.newTask}>
+    <form onSubmit={handleAddNewTask} className={styles.newTask}>
       <input 
         type="text" 
-        placeholder='Adicionar uma nova tarefa' 
+        placeholder='Adicionar uma nova tarefa'
+        value={newTaskText}
+        onChange={handleNewTaskChange}
       />
-      <button>
+
+      <button type='submit'>
         Criar 
         <PlusCircle size={16} />
       </button>
-    </div>
+    </form>
   )
 }
