0ad7e96637c4f3af566e93a41b050fe5ab6c27d7
felipesanderp <felipe.panisson@hotmail.com>
2023-09-26 17:06:00 -0300

    first route to GET tasks

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+import { randomUUID } from 'node:crypto'
+
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: "GET",
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search,
+      } : null)
+
+      return res.end(JSON.stringify(tasks))
+    }
+  }
+]
